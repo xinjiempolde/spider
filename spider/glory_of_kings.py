9ef94952129215c6e46089f1e23ebb9c761756bb
xinjiempolde <2252767663@qq.com>
2018-11-11 11:22:14 +0800

    add images

---
+++ spider/glory_of_kings.py
@@ -0,0 +1,14 @@
+from urllib import request
+import json
+import os
+response=request.urlopen("https://pvp.qq.com/web201605/js/herolist.json")
+hero_json=json.loads(response.read())
+hero_num=len(hero_json)
+hero_name=hero_json[0]['cname']
+skin_names=hero_json[0]['skin_name'].split('|')
+for i in range(hero_num):
+    skin_names=hero_json[i]['skin_name'].split('|')
+    for cnt in range(len(skin_names)):
+        save_file_name='./images'+str(hero_json[i]['ename'])
+        skin_url = 'http://game.gtimg.cn/images/yxzj/img201606/skin/hero-info/' + str(hero_json[i]['ename']) + '/' + str(hero_json[i]['ename']) + '-bigskin-' + str(cnt + 1) + '.jpg'
+        request.urlretrieve(skin_url,save_file_name)
